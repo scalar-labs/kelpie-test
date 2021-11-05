374acedbf36e7ff05ae4cdd5ca497463e9ed2b6b
scalar-boney <58680313+scalar-boney@users.noreply.github.com>
2021-11-05 11:54:58 +0900

    Update the scalardl-test for auditor support (#37)

--- scalardl-test/src/main/java/scalardl/Common.java
+++ scalardl-test/src/main/java/scalardl/Common.java
@@ -1,20 +1,22 @@
 package scalardl;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import com.scalar.dl.client.config.ClientConfig;
-import com.scalar.dl.client.service.ClientModule;
 import com.scalar.dl.client.service.ClientService;
+import com.scalar.dl.client.service.ClientServiceFactory;
 import com.scalar.kelpie.config.Config;
 import io.github.resilience4j.core.IntervalFunction;
 import io.github.resilience4j.retry.Retry;
 import io.github.resilience4j.retry.RetryConfig;
+import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
 public class Common {
   private static String HOST = "localhost";
   private static String PORT = "50051";
+  private static String AUDITOR_HOST = "localhost";
+  private static String AUDITOR_PORT = "40051";
+  private static String AUDITOR_ENABLED = "false";
   private static ClientConfig config;
   private static final String CERT_HOLDER_ID = "test_holder";
   private static final int MAX_RETRIES = 10;
@@ -26,24 +28,36 @@ public class Common {
   public static ClientConfig getClientConfig(Config config) {
     String host = config.getUserString("client_config", "dl_server", HOST);
     String port = config.getUserString("client_config", "dl_server_port", PORT);
+    String auditorEnabled = config.getUserString("client_config", "auditor_enabled", AUDITOR_ENABLED);
+    String auditorHost = config.getUserString("client_config", "auditor_host", AUDITOR_HOST);
+    String auditorPort = config.getUserString("client_config", "auditor_port", AUDITOR_PORT);
     String certificate = config.getUserString("client_config", "certificate");
     String privateKey = config.getUserString("client_config", "private_key");
 
     Properties properties = new Properties();
     properties.setProperty(ClientConfig.SERVER_HOST, host);
     properties.setProperty(ClientConfig.SERVER_PORT, port);
+    properties.setProperty(ClientConfig.AUDITOR_ENABLED, auditorEnabled);
+    properties.setProperty(ClientConfig.AUDITOR_HOST, auditorHost);
+    properties.setProperty(ClientConfig.AUDITOR_PORT, auditorPort);
     properties.setProperty(ClientConfig.CERT_HOLDER_ID, CERT_HOLDER_ID);
     properties.setProperty(ClientConfig.CERT_PATH, certificate);
     properties.setProperty(ClientConfig.PRIVATE_KEY_PATH, privateKey);
 
-    return new ClientConfig(properties);
+    ClientConfig clientConfig = null;
+    try {
+      clientConfig = new ClientConfig(properties);
+    } catch (IOException e) {
+      throw new IllegalArgumentException(e.getMessage());
+    }
+    return clientConfig;
   }
 
   public static ClientService getClientService(Config config) {
     ClientConfig clientConfig = getClientConfig(config);
-    Injector injector = Guice.createInjector(new ClientModule(clientConfig));
+    ClientServiceFactory factory = new ClientServiceFactory(clientConfig);
 
-    return injector.getInstance(ClientService.class);
+    return factory.getClientService();
   }
 
   public static int getTotalInitialBalance(Config config) {
