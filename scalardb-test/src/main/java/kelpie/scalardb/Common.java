94675f18d79991214f86b1433c0045ef92d159ad
yito88 <yito88@protonmail.com>
2026-05-30 15:01:03 +0200

    check isolation level

--- scalardb-test/src/main/java/kelpie/scalardb/Common.java
+++ scalardb-test/src/main/java/kelpie/scalardb/Common.java
@@ -92,6 +92,8 @@ public class Common {
     props.setProperty("scalar.db.storage", storage);
     props.setProperty("scalar.db.transaction_manager", transactionManager);
     props.setProperty("scalar.db.consensus_commit.isolation_level", isolationLevel);
+    logWarn("ISOLATION_LEVEL: " + isolationLevel);
+
     return new DatabaseConfig(props);
   }
 
